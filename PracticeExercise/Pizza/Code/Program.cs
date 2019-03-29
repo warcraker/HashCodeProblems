f55039ec8e4d5b55d135cfc72793cc1d88a65f9c
Guillermo Mestre Carrión <guillermo.mestre.carrion@everis.com>
2019-03-29 07:32:59 +0100

    Example program with points

--- PracticeExercise/Pizza/Code/Program.cs
+++ PracticeExercise/Pizza/Code/Program.cs
@@ -15,9 +15,29 @@ namespace HashPizza
 
             string rootPath = Utils.GetAppRootFolder();
             ProblemFiles files = new ProblemFiles(rootPath);
-            InputFile selectedFile = Utils.SelectInputFile(files); 
+            InputFile inputFile = Utils.SelectInputFile(files);
 
-            First.GenerateFileWithAllSlices(selectedFile.FullPath, Path.Combine(rootPath, "Temp"));
+            Utils.BeginSection("Generate intermediate file");
+            string temporalFilePath = First.GenerateFileWithAllSlices(inputFile.FullPath, Path.Combine(rootPath, "Temp"));
+            Console.WriteLine("..."); Console.ReadLine();
+
+            Utils.BeginSection("Generate solution");
+            string greedySolutionFilePath = Greedy.GenerateGreedySolution(inputFile, temporalFilePath, Path.Combine(rootPath, "Out"));
+            Console.WriteLine("..."); Console.ReadLine();
+
+            Utils.BeginSection("Validate solution");
+            IList<string> errors;
+            bool isValid = Tester.ValidateOutput(inputFile.FullPath, greedySolutionFilePath, out errors);
+            Console.WriteLine($"File is valid: {isValid}");
+            foreach (string error in errors)
+            {
+                Console.WriteLine(error);
+            }
+            Console.WriteLine("..."); Console.ReadLine();
+
+            Utils.BeginSection("Count points");
+            int points = Tester.GetPoints(greedySolutionFilePath);
+            Console.WriteLine($"Total points: {points}");
 
             Utils.EndProgram();
         }
